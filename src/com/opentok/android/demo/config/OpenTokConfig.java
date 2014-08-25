a48dcbbe3f1285659dcf4efe9848fe2cbc698c56
madhavc <mchhura@tokbox.com>
2014-08-25 16:30:16 -0700

    Initial Commit

---
+++ src/com/opentok/android/demo/config/OpenTokConfig.java
@@ -0,0 +1,19 @@
+package com.opentok.android.demo.config;
+
+public class OpenTokConfig {
+	
+	// *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+	// ***                      https://dashboard.tokbox.com/projects                           ***
+	// Replace with a generated Session ID
+	public static final String SESSION_ID = "2_MX4xMDB-MTI3LjAuMC4xflRodSBKdWwgMTAgMTY6MDY6MDMgUERUIDIwMTR-MC45MTQzODg5NX5-";
+	// Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+	public static final String TOKEN = "T1==cGFydG5lcl9pZD0xMDAmc2RrX3ZlcnNpb249dGJwaHAtdjAuOTEuMjAxMS0wNy0wNSZzaWc9NTY1ZWNmYzFmNDdiNDY2NjRhZTJlMzc2MjMzMzM4NDhjZmU1OTQ4YTpzZXNzaW9uX2lkPTJfTVg0eE1EQi1NVEkzTGpBdU1DNHhmbFJvZFNCS2RXd2dNVEFnTVRZNk1EWTZNRE1nVUVSVUlESXdNVFItTUM0NU1UUXpPRGc1Tlg1LSZjcmVhdGVfdGltZT0xNDA1MDMzOTg3JnJvbGU9bW9kZXJhdG9yJm5vbmNlPTE0MDUwMzM5ODcuNDM5NjE1NTE1NDQzOSZleHBpcmVfdGltZT0xNDA3NjI1OTg3"; 
+	// Replace with your OpenTok API key
+	public static final String API_KEY= "100";
+	
+	// Subscribe to a stream published by this client. Set to false to subscribe
+    // to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = false;
+  
+	
+}
